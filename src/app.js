6f14c54fa23f627b926e5826ce15c27ba1de2207
vishalbutolia <vishalbutolia@gmail.com>
2020-05-25 05:07:29 +0530

    setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -4,7 +4,8 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-const app = express()
+const app = express();
+const port = process.env.PORT || 3000;
 
 //Define path for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -88,4 +89,4 @@ app.get('*', (req, res) => {
   });
 });
 
-app.listen(3000, () => console.log('Weather server is up on port 3000'));
+app.listen(port, () => console.log('Weather server is up on port '+ port));
